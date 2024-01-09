e1b8818dde05f8af6a6449086b32f20adeed53d2
ChaoJen <chao.jen.main@gmail.com>
2024-01-09 23:33:30 +0800

    只猜正整數

--- src/week1/Quiz3.sol
+++ src/week1/Quiz3.sol
@@ -5,20 +5,20 @@ pragma solidity ^0.8.13;
  * 3. 開發猜數字的合約(一個人設定，大家猜，有人猜中，就結束不讓其他人猜了)
  */
 interface GuessNumber {
-    function setNumber(int number) external;
-    function guess(int number) external returns (bool);
+    function setNumber(uint number) external;
+    function guess(uint number) external returns (bool);
 }
 
 contract GuessNumberImpl is GuessNumber {
-    int private number;
+    uint private number;
     bool private allowGuess;
 
-    function setNumber(int _number) external override {
+    function setNumber(uint _number) external override {
         number = _number;
         allowGuess = true;
     }
 
-    function guess(int _number) external override returns (bool) {
+    function guess(uint _number) external override returns (bool) {
         require(allowGuess, "Game Over");
 
         if (number == _number) {
